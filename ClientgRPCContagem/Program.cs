1f22423b0cdc73a7afac2d4503dfed5320a2cb9f
Renato Groffe <renato.groffe@yahoo.com.br>
2019-10-01 14:36:10 -0300

    .NET Core 3.0 + Client gRPC contagem de acessos + Protobuf
    .NET Core 3.0 + Client gRPC contagem de acessos + Protobuf

---
+++ ClientgRPCContagem/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using gRPCContagem;
+
+namespace ClientgRPCContagem
+{
+    class Program
+    {
+        private const string SERVER_GRPC = "https://localhost:5001";
+        //private const string SERVER_GRPC = "http://localhost:25001";
+        //private const string SERVER_GRPC = "http://52.186.82.56";
+
+        public static async Task Main()
+        {
+            if (!SERVER_GRPC.StartsWith("https"))
+            {
+                AppContext.SetSwitch(
+                    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            }
+
+            var channel = GrpcChannel.ForAddress(SERVER_GRPC);
+            var client = new ContadorSvc.ContadorSvcClient(channel);
+
+            while (true)
+            {
+                var resultado = await client.GerarValorAsync(
+                    new ContadorRequest()
+                    {
+                        Nome = "Renato"
+                    });
+                Console.WriteLine(
+                    $"| Mensagem: {resultado.Mensagem} " +
+                    $"| Valor Atual: {resultado.ValorAtual} " +
+                    $"| Local: {resultado.LocalSvc} " +
+                    $"| Target Framework: {resultado.TargetFramework} |");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
